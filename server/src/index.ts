b55d8e232d7e4e8d237c73d78e43cd7091ac4214
aleynasarikan <aleynasarikan22@gmail.com>
2026-03-18 19:48:54 +0300

    index.ts update

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,6 +38,12 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+// Debug logger
+app.use((req, res, next) => {
+  console.log(`[DEBUG] Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 // API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
